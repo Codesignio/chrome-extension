3599427c056fc14127dee9ef030b45efe0fd1ceb
bgnorlov <bgnorlov@gmail.com>
2015-12-11 15:07:05 +0300

    change share link block position

--- app/components/select-and-upload.js
+++ app/components/select-and-upload.js
@@ -188,11 +188,10 @@ export default class SelectAndUpload extends React.Component {
         <div key="screenshot"  className="screenshot">
           {this.state.images.concat([]).reverse().map(function(img, i) {
             return (
-              [<div className="image">
+              [img.sharedLink ? [<div className="sharedTitle">Share link and disscuss online:</div>,<input disabled="true" onClick={this.selectInputText.bind(this, img.sharedLink)} className="sharedLink" value={img.sharedLink}/>] : null , <div className="image">
                 <img onMouseOut={this.hideIcon.bind(this, i)} onMouseMove={this.showIcon.bind(this, i)} key={i} src={img.link}/>
                 <div onClick={this.handleRemove.bind(this)} onMouseMove={this.showIcon.bind(this, i)} className="removeIcon" style={{display: this.state.showHideIcon[i] ? 'block' : 'none'}}><div className="icon"></div></div>
-              </div>,
-                img.sharedLink ? [<div className="sharedTitle">Share link and disscuss online:</div>,<input disabled="true" onClick={this.selectInputText.bind(this, img.sharedLink)} className="sharedLink" value={img.sharedLink}/>] : null ]
+              </div>]
             )
 
           }.bind(this)) }
